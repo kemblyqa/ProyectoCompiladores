0c56b4cefd5d6ebf9f2b811755e638e0f9ce9a95
Alberth Salas C <alberthsalascalero@gmail.com>
2018-03-18 15:21:06 -0600

    Parece que todo está listo
    Visitor no es parte de aqui xD

--- src/Editor.java
+++ src/Editor.java
@@ -1,104 +1,57 @@
 import generated.projectParser;
 import generated.projectScanner;
-import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
-import org.antlr.v4.runtime.tree.ParseTree;
-
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
 import javax.swing.*;
 
 public class Editor {
-    private JEditorPane codeEditor;
-    private JTextArea compDetails;
+    JEditorPane codeEditor;
+    JTextArea compDetails;
     private JButton btnCompilar;
     private JButton btnGen;
     private JButton btnCargar;
     private JPanel Principal;
+    private Main creator;
 
-    //variables
-    projectScanner inst = null;
-    projectParser parser = null;
-
-    ANTLRInputStream input=null;
-    CommonTokenStream tokens = null;
-    ParseTree tree = null;
-
+    JFrame frame;
     private void initComponents() {
 
     }
 
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame("Editor");
-        Editor editor = new Editor();
-        editor.btnCargar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    FileDialog dialog = new FileDialog((Frame)null, "Seleccione un archivo .txt");
-                    dialog.setMode(FileDialog.LOAD);
-                    dialog.setFile("*.txt");
-                    dialog.setVisible(true);
-                    String fullpath = dialog.getDirectory() + dialog.getFile();
-                    editor.codeEditor.setText(new Scanner(new File(fullpath)).useDelimiter("\\Z").next());
-                } catch (Exception el) {
-                    editor.compDetails.setText("ERROR: "+el.getMessage()+ "\nCAUSA: "+el.getCause());
-                }
-            }
-        });
-        editor.btnCompilar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    editor.input = new ANTLRInputStream(editor.codeEditor.getText());
-                    editor.inst = new projectScanner(editor.input);
-                    editor.tokens = new CommonTokenStream(editor.inst);
-                    editor.parser = new projectParser(editor.tokens);
-                }
-                catch(Exception exc){
-
-                }
-
-                try {
-                    editor.parser.removeErrorListeners();
-                    editor.parser.addErrorListener(ThrowingErrorListener.INSTANCE);
-                    editor.tree = editor.parser.program();
-                    editor.compDetails.setText("Compilación Exitosa!!" );
-                }
-                catch (ParseCancellationException exc){
-                    editor.compDetails.setText(exc.getMessage());
-                }
-            }
-        });
-        editor.btnGen.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    if (editor.tree ==null){
-                        editor.compDetails.setText("No se ha compilado aún!!");
-                        return;
-                    }
-                    java.util.concurrent.Future <JFrame> treeGUI = org.antlr.v4.gui.Trees.inspect(editor.tree,editor.parser);
-                    treeGUI.get().setVisible(true);
-                    editor.compDetails.setText("Arbol Generado!!\n");
-                }
-                catch (Exception exc){
-                    editor.compDetails.setText("ERROR: "+exc.getMessage()+ "\nCAUSA: "+exc.getCause());
-                }
-            }
-        });
-        frame.setContentPane(editor.Principal);
+    public Editor(Main m) {
+        frame = new JFrame("Editor");
+        frame.setContentPane(this.Principal);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setVisible(true);
+        creator = m;
+        btnCargar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    codeEditor.setText(creator.fileLoad());
+                } catch (FileNotFoundException e1) {
+                    compDetails.setText(e1.getStackTrace().toString());
+                }
+            }
+        });
+        btnCompilar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                creator.compile();
+            }
+        });
+        btnGen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                creator.genTree();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,83 @@
+import generated.projectParser;
+import generated.projectScanner;
+import org.antlr.v4.gui.TreeViewer;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    static projectScanner inst = null;
+    static projectParser parser = null;
+
+    static ANTLRInputStream input=null;
+    static CommonTokenStream tokens = null;
+    static ParseTree tree = null;
+
+    Editor mainView;
+    JFrame treeFrame;
+
+    public String fileLoad() throws FileNotFoundException {
+        FileDialog dialog = new FileDialog((Frame)null, "Seleccione un archivo .txt");
+        dialog.setMode(FileDialog.LOAD);
+        dialog.setFile("*.txt");
+        dialog.setVisible(true);
+        String fullpath = dialog.getDirectory() + dialog.getFile();
+        return new Scanner(new File(fullpath)).useDelimiter("\\Z").next();
+    }
+    public void compile() {
+        try {
+            input = new ANTLRInputStream(mainView.codeEditor.getText());
+            inst = new projectScanner(input);
+            tokens = new CommonTokenStream(inst);
+            parser = new projectParser(tokens);
+            parser.removeErrorListeners();
+            parser.addErrorListener(ThrowingErrorListener.INSTANCE);
+            tree = parser.program();
+            mainView.compDetails.setText("Compilación Exitosa!!" );
+        }
+        catch(Exception exc){
+            mainView.compDetails.setText("ERROR: "+exc.getMessage()+ "\nCAUSA: "+exc.getCause());
+        }
+    }
+    public void genTree() {
+        compile();
+        try {
+            if (treeFrame!=null){
+                treeFrame.setVisible(false);
+                treeFrame.dispose();
+                treeFrame = null;
+            }
+            treeFrame = new JFrame("Arbol Generado");
+            JPanel panel = new JPanel(new BorderLayout(0, 0));
+            TreeViewer viewr = new TreeViewer(Arrays.asList(
+                    parser.getRuleNames()),tree);
+            JScrollPane scrollPane = new JScrollPane(viewr);
+            panel.add(scrollPane, BorderLayout.CENTER);
+            treeFrame.add(panel);
+            treeFrame.setSize(1000,500);
+            treeFrame.setVisible(true);
+        } catch (Exception exc) {
+            mainView.compDetails.setText("ERROR: " + exc.getMessage() + "\nCAUSA: " + exc.getCause());
+        }
+    }
+    public void showMain(){
+        mainView.frame.setVisible(true);
+    }
+    public static void main (String[] args) throws ExecutionException, InterruptedException, FileNotFoundException {
+        Main Principal = new Main();
+        Principal.mainView = new Editor(Principal);
+        Principal.showMain();
+    }
+}
